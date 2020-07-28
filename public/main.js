ff4bb224b7a7b3a726f5caeb84fce387ec3d0a6b
Glitch (chatmodguild) <none>
2020-07-28 21:18:18 +0000

    🕷🎉 Checkpoint ./public/main.js:18135371/40 ./public/index.html:18135371/80

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+var userip
 $(function() {
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
@@ -53,7 +54,7 @@ $(function() {
 
   // Sends a chat message
   function sendMessage () {
-    var message = $inputMessage.val();
+    var message = $inputMessage.val()+;
     // Prevent markup from being injected into the message
     message = cleanInput(message);
     // if there is a non-empty message and a socket connection
