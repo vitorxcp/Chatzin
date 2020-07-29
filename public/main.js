602cdf87551d2427ec914b536ac82f5a59f4d6de
Glitch (chatmodguild) <none>
2020-07-29 13:13:56 +0000

    💖🌘 Checkpoint ./public/main.js:18135371/784

--- public/main.js
+++ public/main.js
@@ -2,7 +2,11 @@ $(function() {
   const id = " #"+Math.floor(Math.random()*9)+""+Math.floor(Math.random()*9)+""+Math.floor(Math.random()*9)+""+Math.floor(Math.random()*9)
   var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
-  var COLORS = "black"
+  var COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  ];
   
   document.addEventListener('keydown',function(e){
     if(e.keyCode === 167){
