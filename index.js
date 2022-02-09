1efb5b7154591ba843edbe474c9c85aeec6e10d3
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2022-02-09 19:55:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Setup basic express server
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -9,19 +8,15 @@ server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-// Routing
 app.use(express.static('public'));
 
-// Chatroom
 
 var numUsers = 0;
 
 io.on('connection', function (socket) {
   var addedUser = false;
 
-  // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
     socket.broadcast.emit('new message', {
       username: socket.username,
     //   usering: socket.usering,
@@ -29,11 +24,9 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username, usering) {
     if (addedUser) return;
 
-    // we store the username in the socket session for this client
     socket.username = username;
     socket.usering = usering;
     ++numUsers;
@@ -41,7 +34,6 @@ io.on('connection', function (socket) {
     socket.emit('login', {
       numUsers: numUsers
     });
-    // echo globally (all clients) that a person has connected
     socket.broadcast.emit('user entrou', {
       username: socket.username,
      //  usering: socket.usering,
@@ -49,7 +41,6 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
     socket.broadcast.emit('typing', {
       username: socket.username,
@@ -57,7 +48,6 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function () {
     socket.broadcast.emit('stop typing', {
       username: socket.username,
@@ -65,12 +55,10 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the user disconnects.. perform this
   socket.on('disconnect', function () {
     if (addedUser) {
       --numUsers;
 
-      // echo globally that this client has left
       socket.broadcast.emit('user saiu', {
         username: socket.username,
      //  usering: socket.usering,
