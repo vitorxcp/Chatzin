41d7360e78e72aff36fee3d320daaa12f5944713
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2022-02-09 19:54:38 -0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -5,8 +5,8 @@ $(function() {
   }
   const id = localStorage.i
  
-  var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
+  var FADE_TIME = 150; 
+  var TYPING_TIMER_LENGTH = 400; 
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
@@ -15,15 +15,14 @@ $(function() {
 
   
   var countUser
-  // Initialize variables
   var $window = $(window);
-  var $usernameInput = $('.usernameInput'); // Input for username
-  var $messages = $('.messages'); // Messages area
-  var $inputMessage = $('.inputMessage'); // Input message input box
-  var $loginPage = $('.login.page'); // The login page
-  var $chatPage = $('.chat.page'); // The chatroom page
+  var $usernameInput = $('.usernameInput');
+  var $messages = $('.messages');
+  var $inputMessage = $('.inputMessage')
+  var $loginPage = $('.login.page'); 
+  var $chatPage = $('.chat.page'); 
 
-  // Prompt for setting a username
+
   var username;
   var usering;
   var username2;
@@ -45,7 +44,6 @@ $(function() {
    // log(message);
   }
 
-  // Sets the client's username
   function setUsername () {
     username = cleanInput($usernameInput.val().trim()+id);
      username2 = cleanInput($usernameInput.val().trim());
@@ -54,7 +52,6 @@ usering = "user"
       username = "Visitante"+id
     }
 
-    // If the username is valid
     if (username) {
       $loginPage.fadeOut();
       $chatPage.show();
@@ -93,13 +90,9 @@ if(username2 === "Pedro M [MEMBRO]") username2 = a+` [VIP++]`
   var membros;
 
 
-  // Sends a chat message
   function sendMessage () {
     var message = $inputMessage.val();
       var message1 = $inputMessage.val();
-    // Prevent markup from being injected into the message
-   // message = cleanInput(message);
-    // if there is a non-empty message and a socket connection
      	const ping = new Date();
 	ping.setHours(ping.getHours() - 3);
 	const hora = ping.getHours();
@@ -180,12 +173,10 @@ return  addChatMessage({
           message: "WebSocket API Ping: ...ms. | Site API Ping: ...ms."
         });
       }
-      // tell server to execute 'new messages' and send along one parameter
       socket.emit('new message', message);
     }
   }
 
-  // Log a message
   function log (message, options) {
     var el = ('<div id="jumplogs" class="log">'+message+'<li>');
      var $el2 = $('<br>').text(" ");
@@ -193,9 +184,7 @@ return  addChatMessage({
   }
   
 
-  // Adds the visual chat message to the message list
   function addChatMessage  (data, options) {
-    // Don't fade the message in if there is an 'X was typing'
     var $typingMessages = getTypingMessages(data);
     options = options || {};
     if ($typingMessages.length !== 0) {
@@ -238,29 +227,21 @@ var $errorss = $("<br>")
       addMessageElement($errorss, options)
   }
 
-  // Adds the visual chat typing message
   function addChatTyping (data) {
     data.typing = true;
     data.message = 'Está digitando...';
     addChatMessage(data);
   }
 
-  // Removes the visual chat typing message
   function removeChatTyping (data) {
     getTypingMessages(data).fadeOut(function () {
       $(this).remove();
     });
   }
 
-  // Adds a message element to the messages and scrolls to the bottom
-  // el - The element to add as a message
-  // options.fade - If the element should fade-in (default = true)
-  // options.prepend - If the element should prepend
-  //   all other messages (default = false)
   function addMessageElement (el, options) {
     var $el = $(el);
 
-    // Setup default options
     if (!options) {
       options = {};
     }
@@ -271,7 +252,6 @@ var $errorss = $("<br>")
       options.prepend = false;
     }
 
-    // Apply options
     if (options.fade) {
       $el.hide().fadeIn(FADE_TIME);
     }
@@ -283,12 +263,11 @@ var $errorss = $("<br>")
     $messages[0].scrollTop = $messages[0].scrollHeight;
   }
 
-  // Prevents input from having injected markup
   function cleanInput (input) {
     return $('<div/>').text(input).text();
   }
 
-  // Updates the typing event
+
   function updateTyping () {
     if (connected) {
       if (!typing) {
@@ -308,17 +287,14 @@ var $errorss = $("<br>")
     }
   }
 
-  // Gets the 'X is typing' messages of a user
+
   function getTypingMessages (data) {
     return $('.typing.message').filter(function (i) {
       return $(this).data('username') === data.username;
        return $(this).data('username') === data.usering;
     });
   }
-//  <script type="text/javascript" src="https://l2.io/ip.js?var=userip"></script>
-  // Gets the color of a username through our hash function
   function getUsernameColor (username, usering) {
-    // Compute hash code
     var hash = 7;
     for (var i = 0; i < usering.length; i++) {
        hash = usering.charCodeAt(i) + (hash << 5) - hash;
@@ -326,7 +302,6 @@ var $errorss = $("<br>")
     for (var i = 0; i < username.length; i++) {
        hash = username.charCodeAt(i) + (hash << 5) - hash;
     }
-    // Calculate color
     var index = Math.abs(hash % COLORS.length);
     return COLORS[index];
   }
@@ -334,14 +309,11 @@ function amsfd (){
    setUsername()
       membros = [""+username2+"(Você)", ]
    }
-  // Keyboard events
 
   $window.keydown(function (event) {
-    // Auto-focus the current input when a key is typed
     if (!(event.ctrlKey || event.metaKey || event.altKey)) {
       $currentInput.focus();
     }
-    // When the client hits ENTER on their keyboard
     if (event.which === 13) {
       if (username) {
         sendMessage();
@@ -357,25 +329,16 @@ function amsfd (){
     updateTyping();
   });
 
-  // Click events
-
-  // Focus input when clicking anywhere on login page
   $loginPage.click(function () {
     $currentInput.focus();
   });
 
-  // Focus input when clicking on the message input's border
   $inputMessage.click(function () {
     $inputMessage.focus();
   });
 
-  // Socket events
-
-  // Whenever the server emits 'login', log the login message
   socket.on('login', function (data) {
-    // membros.push(data.username)
     connected = true;
-    // Display the welcome message
         	const ping = new Date();
 	ping.setHours(ping.getHours());
 	const hora = ""+ping.getHours()+""
@@ -395,16 +358,11 @@ function amsfd (){
     membros.splice(as, 1)
   });
 
-  // Whenever the server emits 'new messages', update the chat body
   socket.on('new message', function (data) {
      membros.push(data.username)
     addChatMessage(data);
   });
-  
-  // Whenever the server emits 'alerta', log it in the chat body
-  
 
-  // Whenever the server emits 'user joined', log it in the chat body
   socket.on('user entrou', function (data) {
     amsfd()
      let coloruser = "#929191" 
@@ -417,7 +375,7 @@ function amsfd (){
     
   });
 
-  // Whenever the server emits 'user left', log it in the chat body
+ 
   socket.on('user saiu', function (data) {
     amsfd()
     if(data.username === "Guest"){
@@ -434,13 +392,11 @@ function amsfd (){
     removeChatTyping(data);
   });
 
-  // Whenever the server emits 'typing', show the typing message
   socket.on('typing', function (data) {
    //  membros.push(data.username)
     addChatTyping(data);
   });
 
-  // Whenever the server emits 'stop typing', kill the typing message
   socket.on('stop typing', function (data) {
    //  membros.push(data.username)
     removeChatTyping(data);
